65ae26afdd8b3fca4ebf818b2a9fa986434cc34b
Yin Long <blue.rose.hut@gmail.com>
2015-09-19 15:24:16 +0700

    first commit

---
+++ app/controllers/static_pages_controller.rb
@@ -0,0 +1,16 @@
+class StaticPagesController < ApplicationController
+  def home
+  end
+
+  def product
+  end
+
+  def introduction
+  end
+
+  def partners
+  end
+
+  def contact
+  end
+end
---
+++ test/controllers/static_pages_controller_test.rb
@@ -0,0 +1,29 @@
+require 'test_helper'
+
+class StaticPagesControllerTest < ActionController::TestCase
+  test "should get home" do
+    get :home
+    assert_response :success
+  end
+
+  test "should get product" do
+    get :product
+    assert_response :success
+  end
+
+  test "should get introduction" do
+    get :introduction
+    assert_response :success
+  end
+
+  test "should get partners" do
+    get :partners
+    assert_response :success
+  end
+
+  test "should get contact" do
+    get :contact
+    assert_response :success
+  end
+
+end
